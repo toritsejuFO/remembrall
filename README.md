1b1ad984a3f3c89ecf838852d96fa8c59f2a0604
Faith Odonghanro <toriboi.fo@gmail.com>
2018-11-08 13:13:49 +0100

    Modify js code structure; Update README.md

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-Remembrall
+# Remembrall
 
-Okay! This name is inspired from the movie Harry Potter...if you know you know. It's an SPA, built with Javascript. 
-It randomly(always) brings out 6 colors in sets of two(2) boxes out of 12, so you get a few secs glance at it, and have to match it as it is.
-This process  continues, for as long as you'd like. Contributions would be appreciated.
+Name is inspired from the movie Harry Potter. This is just a game that tests your ability to recall, it helps you flex your memory.
+
+# How to play
+It generates 6 colors in sets of two(2) boxes out of 12 randomly. So you get a few seconds glance at it, and have to match it as it is.
+This process continues, for as long as you'd like.
+
+Head over to https://toritsejufo.github.io/Remembrall/ to play
--- main.js
+++ main.js
@@ -1,130 +1,197 @@
+/**************** MINI CLASS TO MODEL CARD OBJECT ******************/
 
-box = document.getElementsByClassName("all");
+function Box(id, color) {
+    this.id = id;
 
-score = document.getElementsByTagName("p");
+    this.html = document.createElement('div')
+    this.html.id = this.id
+    this.html.style.backgroundColor = color;
+
+    this.hide = (hideColor) => {
+        this.html.style.backgroundColor = hideColor;
+    }
+
+    this.show = (showColor) => {
+        this.html.style.backgroundColor = showColor;
+    }
+}
+
+function Card(id, color = 'gray') {
+    return new Box(id, color);
+}
+
+
+
+/********************** VARIABLES TO MAINTAIN STATE ***********************/
+
+let cardColorMap = new Array(12);
+let cardColorCount = [0, 0, 0, 0, 0, 0];
+let cardClicked = [0, 0, 0, 0];
+
+let cardColors = ["black", "red", "green", "indigo", "blue", "yellow"];
+
+let noOfCards = 12;
+let cards = []
 
 secs = 2000;
 
-bc = new Array(12);
-bci = [0,0,0,0,0,0];
-b_clicked = [0,0,0,0];
+score = document.getElementsByTagName("p");
 
-colors = ["black", "red", "green", "indigo", "blue", "yellow"];
-
-var i;
 var correct = 0;
 var failure_msg = "Sorry, you failed. Try again";
 var success_msg = "Think u're good, go again, see if u can make it 5 in a row, you already have 1";
 
-function rand(num){
+
+
+
+/************ HELPER FUNCTIONS TO HANDLE/MAINTAIN STATE ******************/
+
+rand = (num) => {
     return Math.floor(Math.random() * num);
 }
 
-function unflip(){
-    for(i=0; i<box.length; i++){
-        box[i].style.backgroundColor = "gray";
-    }
-}
-
-function flip_all(){
-    for(i=0; i<box.length; i++){
-        do{
+showCards = () => {
+    cards.forEach(card => {
+        do {
             x = rand(6);
-        }while( bci[x] == 2 );
+        } while (cardColorCount[x] == 2);
 
-        bci[x] += 1;
-        box[i].style.backgroundColor = colors[x];
-        bc[i] = colors[x];
-    }
+        cardColorCount[x] += 1;
+        cardColorMap[card.id] = cardColors[x];
+
+        card.show(cardColors[x]);
+    })
 }
 
-function flip( id ){
-    if( box[id].style.backgroundColor !== "gray" ){
+hideCards = () => {
+    cards.forEach(card => {
+        card.hide('gray');
+    })
+}
+
+clikt = (e) => {
+    if (!e.target.matches('div.all')) return;
+    card = cards[e.target.id];
+    flip(card.id)
+    console.log(card.show(cardColorMap[card.id]));
+}
+
+showIntructions = () => {
+    alert("You have " + (secs / 1000) + " secs to glance at the color arrangement. If u pick 2 consecutive colors that are the same u score a point, else if u pick a two different colors, you have lost and the game resets");
+    alert("Click OK when you're ready");
+}
+
+goAgain = (msg, reset) => {
+    cardColorMap = new Array(12);
+    cardColorCount = [0, 0, 0, 0, 0, 0];
+    cardClicked = [0, 0, 0, 0];
+    if (msg) {
+        alert(msg);
+    }
+    alert("Click OK when you're ready");
+    if (reset) {
+        correct = 0;
+    }
+    hideCards();
+    showCards();
+    setTimeout(hideCards, secs);
+}
+
+
+/**************** START GAME *****************/
+
+window.onload = () => {
+
+    /**
+     * Instantiate cards; Hidden by default
+     */
+    for (let i = 0; i < noOfCards; i++) {
+        cards[i] = Card(i, 'gray');
+    }
+
+    /**
+     * Add cards to html page
+     */
+    container = document.getElementById("container");
+    cards.forEach(function (card) {
+        card.html.className = 'all';
+        container.appendChild(card.html);
+    })
+    container.addEventListener('click', clikt);
+
+    /* Show game instructions and show cards after 1 second */
+    setTimeout(() => {
+        showIntructions()
+        showCards()
+    }, secs / 2);
+
+    /** Hide cards 2 seconds after cards are shown */
+    setTimeout(hideCards, (secs / 2) * 2);
+
+    // PLAY
+}
+
+/**************************************************************/
+
+function flip(id) {
+    if (cards[id].html.style.backgroundColor !== "gray") {
         return;
     }
 
-    box[id].style.backgroundColor = bc[id];
+    cards[id].html.style.backgroundColor = cardColorMap[id];
 
-    if(b_clicked[0] == 0){
-        b_clicked[0] = 1;
-        b_clicked[2] = id;
+    if (cardClicked[0] == 0) {
+        cardClicked[0] = 1;
+        cardClicked[2] = id;
     }
-    else if((b_clicked[0] == 1) && (b_clicked[1] == 0)){
-        b_clicked[1] = 1;
-        b_clicked[3] = id;
+    else if ((cardClicked[0] == 1) && (cardClicked[1] == 0)) {
+        cardClicked[1] = 1;
+        cardClicked[3] = id;
     }
 
-    if((b_clicked[0] == 1) && (b_clicked[1] == 1)){
-        if( box[b_clicked[2]].style.backgroundColor ==  box[b_clicked[3]].style.backgroundColor ){
+    if ((cardClicked[0] == 1) && (cardClicked[1] == 1)) {
+        if (cards[cardClicked[2]].html.style.backgroundColor == cards[cardClicked[3]].html.style.backgroundColor) {
             correct += 10;
             score[0].innerHTML = "Score: " + correct;
-            if( correct % 60 == 0){
+            if (correct % 60 == 0) {
                 alert("Superb, you got all correctly");
-                switch(correct){
-                    case 60: times = 5;break;
-                    case 120: times = 4;break;
-                    case 180: times = 3;break;
-                    case 240: times = 2;break;
-                    case 300: times = 1;break;
+                switch (correct) {
+                    case 60: times = 5; break;
+                    case 120: times = 4; break;
+                    case 180: times = 3; break;
+                    case 240: times = 2; break;
+                    case 300: times = 1; break;
                 }
                 times -= 1;
 
-                if(times == 4){
-                goAgain(success_msg);
+                if (times == 4) {
+                    goAgain(success_msg);
                 }
-                else if(times != 0){
-                if(times < 0){times = 5}
-                goAgain("Remaining " + times + " times in a row");
-                }else{
+                else if (times != 0) {
+                    if (times < 0) { times = 5 }
+                    goAgain("Remaining " + times + " times in a row");
+                } else {
                     goAgain("You have proven yourself");
                 }
 
             }
         }
-        else{
+        else {
             t = Math.floor(correct / 60);
-            if(t < 5 && t >= 1){
-            t += 1;
+            if (t < 5 && t >= 1) {
+                t += 1;
                 alert("Tough luck, you almost made it " + t + " time(s)");
             }
-            else if(t >= 5){
+            else if (t >= 5) {
                 alert("Wow, you did it, you really are good");
                 goAgain("You could keep going", 1);
             }
             goAgain(failure_msg, 1);
         }
 
-        for(i=0; i<4; i++){
-            b_clicked[i] = 0;
+        for (i = 0; i < 4; i++) {
+            cardClicked[i] = 0;
         }
     }
 
-}
-
-window.onload = function(){
-    unflip();
-
-    alert("You have " + (secs / 1000) + " secs to glance at the color arrangement. If u pick 2 consecutive colors that are the same u score a point, else if u pick a two different colors, you have lost and the game resets");
-    alert("Click OK when you're ready");
-
-    flip_all();
-
-    setTimeout(unflip, secs);
-
-}
-
-function goAgain(msg, reset){
-    if(msg){
-        alert(msg);
-    }
-    alert("Click OK when you're ready");
-    bc = new Array(12);
-    bci = [0,0,0,0,0,0];
-    b_clicked = [0,0,0,0];
-    if(reset){
-        correct = 0;
-    }
-    unflip();
-    flip_all();
-    setTimeout(unflip, secs);
 }
